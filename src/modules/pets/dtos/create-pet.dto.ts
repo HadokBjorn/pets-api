e26dd617ec6f9bd9f7ffcbc1181d7e2f85ebcbe1
Esaú Bandeira <esaubandeira.dev@gmail.com>
2025-09-12 22:14:26 -0500

    feat add pets dto

---
+++ src/modules/pets/dtos/create-pet.dto.ts
@@ -0,0 +1,11 @@
+import { PetProperties } from "../enums/pet.enum";
+
+export class CreatePetDto {
+  name: string;
+  specie: PetProperties.Specie;
+  breed: string;
+  ageInMonths: number;
+  gender: PetProperties.Gender;
+  status: PetProperties.Status;
+  locationId: string;
+}
