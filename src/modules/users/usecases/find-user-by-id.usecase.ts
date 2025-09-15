109120a23aa9c496d030160d7efc9f0203159154
Esaú Bandeira <esaubandeira.dev@gmail.com>
2025-09-15 17:30:02 -0500

    refactor: add not found exception in find user by id use case

--- src/modules/users/usecases/find-user-by-id.usecase.ts
+++ src/modules/users/usecases/find-user-by-id.usecase.ts
@@ -1,8 +1,13 @@
+import { NotFoundException } from "@/shared/exceptions";
 import prismaUsersRepository from "../repositories/impl/prisma-users.repository";
 
 class FindUserByIdUseCase {
   async execute(id: string) {
     const user = await prismaUsersRepository.find({ id });
+
+    if (!user) {
+      throw new NotFoundException("User not found");
+    }
 
     delete user.password;
 
